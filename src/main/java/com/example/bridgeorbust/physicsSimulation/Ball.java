d1515af0cd4e8cb0dc7afc0110d94e92d15113cc
Noah <noahpoul@live.ca>
2025-03-19 12:03:03 -0400

    waza

--- src/main/java/com/example/bridgeorbust/physicsSimulation/Ball.java
+++ src/main/java/com/example/bridgeorbust/physicsSimulation/Ball.java
@@ -53,29 +53,10 @@ public class Ball {
     }
 
     void checkCollisions(Beam beam) {
-
-
-
-        Beam overBeam;
-        double deltaX, deltaY, relativePosition, ratio, adjustment, pinDistance,relativeHeight;
-
         if ((this.position.x > beam.pin1.getPosition().x && this.position.x < beam.pin2.getPosition().x)
                 || (this.position.x <= beam.pin1.getPosition().x && this.position.x >= beam.pin2.getPosition().x)) {
             if (this.position.y >= beam.pin1.getPosition().y -this.radius || this.position.y >= beam.pin2.getPosition().y - (this.radius) ) {
-                if(trulyUnder(beam)){
-
-                }
-//                overBeam = beam;
-//                deltaX =  overBeam.pin2.getPosition().x- overBeam.pin1.getPosition().x;
-//                deltaY = overBeam.pin1.getPosition().y - overBeam.pin2.getPosition().y;
-//                relativePosition = this.position.x - overBeam.pin1.getPosition().x;
-//                ratio = relativePosition / deltaX;
-//                pinDistance = this.position.y - overBeam.pin1.getPosition().y;
-//                relativeHeight=(deltaX>=0)?deltaY*ratio:-(deltaY*ratio);
-//                adjustment = (deltaY >= 0) ? pinDistance + relativeHeight : pinDistance+deltaY+relativeHeight;
-////                car.pin2.setY(car.pin1.getPosition().y -adjustment - 15);
-//                this.position.y= this.position.y - adjustment - this.radius;
-//                System.out.println("adj="+adjustment);
+                if(trulyUnder(beam)){}
             }
         }
     }
@@ -87,17 +68,9 @@ public class Ball {
             followingPinX=beam.pin1;
         }
         else {
-            leadingPinX=beam.pin1;
-            followingPinX=beam.pin2;
+            leadingPinX = beam.pin1;
+            followingPinX = beam.pin2;
         }
-//        if(beam.pin1.getPosition().y>=beam.pin2.getPosition().y){
-//            leadingPinY=beam.pin2;
-//            followingPinY=beam.pin1;
-//        }
-//        else {
-//            leadingPinY=beam.pin1;
-//            followingPinY=beam.pin2;
-//        }
         deltaX=followingPinX.getPosition().x-leadingPinX.getPosition().x;
         deltaY=followingPinX.getPosition().y-leadingPinX.getPosition().y;
         relativeLength=this.position.x+this.radius-leadingPinX.getPosition().x;
@@ -111,14 +84,6 @@ public class Ball {
         else
             return false;
     }
-//    public void setX(double x){
-//        this.position=new Vector2D(x, position.y);
-//
-//    }
-//    public void setY(double y){
-//        this.position=new Vector2D(position.x, y);
-//
-//    }
 
     public Vector2D getPosition() {
         return position;
